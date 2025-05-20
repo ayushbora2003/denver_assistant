03b8a714cb3a01b5b3fc2a4d06d72f889f1910f0
ayushbora2003 <ayushbora2003@gmail.com>
2025-05-20 13:32:10 +0530

    day_10

--- engine/db.py
+++ engine/db.py
@@ -7,13 +7,16 @@ cursor=con.cursor()
 query="CREATE TABLE IF NOT EXISTS sys_command(id integer primary key,name VARCHAR(100),path VARCHAR(1000))"
 cursor.execute(query)
 
-# query="INSERT INTO sys_command VALUES(null,'','')"
-# cursor.execute(query)
-con.commit()
+query = "INSERT INTO sys_command VALUES (?, ?, ?)"
+values  = (None, 'vlc', r'C:\\Program Files (x86)\\VideoLAN\\VLC.exe')
+
+# cursor.execute(query, values)
+#con.commit()
+
 
 #web_commands:-----
-# query="CREATE TABLE IF NOT EXISTS web_command(id integer primary key,name VARCHAR(100),url VARCHAR(1000))"
-# cursor.execute(query)
+query="CREATE TABLE IF NOT EXISTS web_command(id integer primary key,name VARCHAR(100),url VARCHAR(1000))"
+cursor.execute(query)
 
 # query="INSERT INTO web_command VALUES(null,'google','https://www.google.com/')"
 # cursor.execute(query)
@@ -21,6 +24,6 @@ con.commit()
 
 
 #delete :------
-# query = "DELETE FROM web_command WHERE name = 'canva' AND url = 'https://www.youtube.com/'"
-# cursor.execute(query)
-# con.commit()
+query = r"DELETE FROM sys_command WHERE name = 'vlc' AND path = 'C:\Program Files (x86)\VideoLAN\VLC.exe'"
+cursor.execute(query)
+con.commit()
